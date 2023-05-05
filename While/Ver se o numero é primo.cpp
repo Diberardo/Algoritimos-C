d181985e8e35f7970f78fdc9173e609eb6137e93
Giulio <126623979+Diberardo@users.noreply.github.com>
2023-05-05 15:59:18 -0300

    Update Ver se o numero é primo.cpp

--- While/Ver se o numero é primo.cpp
+++ While/Ver se o numero é primo.cpp
@@ -11,17 +11,17 @@ main()
 	    printf("\nNumero = "); scanf("%d",&n);
     }while ( n < 0 );
      
-    if ( n < 2 ) p = 0;           // P é um flag     0 == nao primo  // 1 == primo 
-    else p = 1;                       // tem que iniciar como primo ver se é
+    if ( n < 2 ) p = 0;           // P Ã© um flag     0 == nao primo  // 1 == primo 
+    else p = 1;                       // tem que iniciar como primo ver se Ã©
     
     while ( i <= sqrt(n))
     {
-    	if ( n % i == 0 )
+    	 if ( n % i == 0 )
     	{
     		p = 0;
     		break;       
-		}
-		else i++;
+	}
+	 else i++;
 	}
 	if ( p == 1 ) printf ("\n %d e primo",n); 
 	else printf("\n %d nao e primo",n);
