0d103dce4910ad29552663336442c37c268c8a56
Giulio <126623979+Diberardo@users.noreply.github.com>
2023-06-09 15:19:17 -0300

    Ver Como o Computador se Comporta

--- Algoritmos Simples/1.15.cpp
+++ Algoritmos Simples/1.15.cpp
@@ -9,5 +9,4 @@ main()
 	printf("\n3!=3  : %d" , 3!=3);     // Ver Como o Computador se comporta, FALSO e VERDADEIRO , O E 1;  
 	printf("\n7>=5  : %d" , 7>=5);
 	printf("\n7<=5  : %d" , 7<=5);
-	
 }
