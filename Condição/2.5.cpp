7e9e0ec7edf50cae13712320749f9b63f41e9b88
Giulio <126623979+Diberardo@users.noreply.github.com>
2023-06-14 15:13:13 -0300

    Produto tem um Desconto sobre seu Preço

--- Condição/2.5.cpp
+++ Condição/2.5.cpp
@@ -1,20 +1,20 @@
+//Um produto tem um desconto sobre seu preço, considerando o dia do mês em 
+//que foi comprado: se for na primeira quinzena, o desconto é de 8%; se for na 
+//segunda quinzena, o desconto é de 6%. Mostre o preço do produto com desconto.
+
 #include<stdio.h>
-
 main()
-
 {
 	float P, PF;
 	int D;
 	
 	printf("digite o valor do produto = ");
 	scanf("%f",&P);
-	
 	printf("digite o dia que voce comprou = ");
 	scanf("%d",&D);
 	
 	 if (D<=15)
 	 	PF = P * 0.92;
-    
 	 else 
 	     PF = P * 0.94;
 	 
