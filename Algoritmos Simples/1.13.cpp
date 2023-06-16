d7260c7e8937ba6774d7c2f8756be4f7b6e68124
Giulio <126623979+Diberardo@users.noreply.github.com>
2023-06-16 15:22:01 -0300

    .

--- Algoritmos Simples/1.13.cpp
+++ Algoritmos Simples/1.13.cpp
@@ -1,17 +1,12 @@
-//Um funcionário recebe um salário mensal, que deve ter 15% de desconto e um 
-//acréscimo de 100 reais. Exibir o salário final após os cálculos.
 #include<stdio.h>
 main()
-
 {
 	float sal, sf , des;
 	
-	 printf("\ndigite seu salario atual = ");
-	 scanf("%f" , &sal);
+	 printf("\ndigite seu salario atual = "); scanf("%f" , &sal);
 	
 	  des = sal * 0.15;
 	  sf = sal - des + 100;
-	  
 	  
 	 printf("\nSeu salário com desconto = %f" , sf); 
 }
