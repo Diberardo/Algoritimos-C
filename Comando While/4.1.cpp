5c3900de084340659dd8ca6260293b21abb3879c
Giulio <126623979+Diberardo@users.noreply.github.com>
2023-06-16 15:34:49 -0300

    .

---
+++ Comando While/4.1.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+
+{
+	int A1, A2;
+	do
+	{
+	printf("\nAno 1 = "); scanf("%d",&A1);
+	printf("\nAno 2 = "); scanf("%d",&A2);
+	} while ( A1 <= 0 || A2 <= 0) ;
+	
+    while( A1 <= A2 ) 
+    {
+    	if((A1 % 4 == 0 && A1 % 100 != 0) || (A1 % 400 == 0)) 
+	  printf("%d\n",A1);
+         
+	A1++;
+    }
+}
