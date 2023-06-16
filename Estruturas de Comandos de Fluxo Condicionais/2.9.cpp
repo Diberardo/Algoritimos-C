61bf3bb51f039a487e9ddf61e36bc08d35657413
Giulio <126623979+Diberardo@users.noreply.github.com>
2023-06-16 15:17:09 -0300

    .

--- Estruturas de Comandos de Fluxo Condicionais/2.9.cpp
+++ Estruturas de Comandos de Fluxo Condicionais/2.9.cpp
@@ -1,6 +1,3 @@
-//Escreva um programa em C que leia uma idade de uma pessoa e verifique se ela é maior de idade ou não. 
-//Considere a idade mínima para ser maior de idade como 18 anos.
-
 #include<stdio.h>
 main()
 {
