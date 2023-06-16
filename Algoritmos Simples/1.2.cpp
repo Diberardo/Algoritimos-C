58069c764ee78a05453ee51b2ac5b0c3a3d469f7
Giulio <126623979+Diberardo@users.noreply.github.com>
2023-06-16 15:22:50 -0300

    .

--- Algoritmos Simples/1.2.cpp
+++ Algoritmos Simples/1.2.cpp
@@ -1,8 +1,6 @@
-// Digite um Salário e de um aumento de 10% e mostre o salário final ?
 #include<stdio.h>
 main()
 {
-	
 	float sal, sf;
 	
 	 printf("digite seu salario atual = ");
@@ -11,7 +9,5 @@ main()
 	  sf = sal*1.1; // da pra fazer outro jeito: ====>   aum = sal * 0.1   =>   sf= sal+aum 
 	 
 	 printf("\nseu salario antigo era = %f " , sal);
-	 printf("\nSeu Salario com aumento é = %f", sf);
-	   
-	 
+	 printf("\nSeu Salario com aumento é = %f", sf); 
 }
