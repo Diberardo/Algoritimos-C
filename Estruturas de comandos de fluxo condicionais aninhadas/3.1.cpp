4848b898bd8235fb030fcae2a18acec235dffa0c
Giulio <126623979+Diberardo@users.noreply.github.com>
2023-06-16 15:11:18 -0300

    .

--- Estruturas de comandos de fluxo condicionais aninhadas/3.1.cpp
+++ Estruturas de comandos de fluxo condicionais aninhadas/3.1.cpp
@@ -6,12 +6,12 @@ int main()
     printf("\nAno = ");
     scanf("%d",&A);
     if ((A % 4 == 0 && A % 100 != 0) || (A % 400 == 0)) 
-	{
+    {
         printf("\nBissexto");
     } 
 	else 
 	{
-        printf("\nNao e bissexto");
-    }
+          printf("\nNao e bissexto");
+        }
     return 0;
 }
