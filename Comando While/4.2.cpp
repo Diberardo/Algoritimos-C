ab95b8cbfb371407b0300049455cb24a459397de
Giulio <126623979+Diberardo@users.noreply.github.com>
2023-06-16 15:41:28 -0300

    .

--- Comando While/4.2.cpp
+++ Comando While/4.2.cpp
@@ -1,14 +1,14 @@
-// Faça um programa em C para solicitar a entrada de um número positivo x. Crie uma repetição para somar todos os números ímpares de 1 até x. 
-//Mostre o valor da soma obtida.
 #include<Stdio.h>
-
 main()
 {
 	int A,B,X;
+	do
+	{
 	printf("\n Numero que sera divisivel = "); scanf("%d",&X);
 	printf("\n Limite A = "); scanf("%d",&A);
 	printf("\n Limite B = "); scanf("%d",&B);
-	                                                 // O B tem que ser o numero maior 
+	} while ( A < 0 || B <= 0 || X <= 0 );      // O B tem que ser o numero maior 
+	
 	printf("\n Numero que vai dividir %d limite A %d limite B %.2d:",X,A,B);
 	int cont = A;
 	
