443c4390088e35212f82a8da2cc9d648fbfa253f
Giulio <126623979+Diberardo@users.noreply.github.com>
2023-06-16 15:22:32 -0300

    .

--- Algoritmos Simples/1.14.cpp
+++ Algoritmos Simples/1.14.cpp
@@ -1,15 +1,11 @@
-//Faça um programa que calcula o valor total de uma compra com base no preço de um produto e na quantidade desejada pelo usuário ?
 #include<stdio.h>
 main()
-
 {
 	int P ,Q, T;
 	
-	 printf("\ndigite valor produto = ");
-	 scanf("%d",&P);
+	 printf("\ndigite valor produto = "); scanf("%d",&P);
 	
-	 printf("\ndigite quantidade = ");
-	 scanf("%d",&Q);
+	 printf("\ndigite quantidade = "); scanf("%d",&Q);
 	
 	  T = P * Q;
 	  
