053eab32cf5232ba21152a2c1ffd5c339678ad8d
Giulio <126623979+Diberardo@users.noreply.github.com>
2023-06-20 11:53:02 -0300

    .

---
+++ Comando While/4.5.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main ()
+{
+	int i = 1, r,T;
+	do
+	{
+	 printf("\nDigite o Numero para ver a tabuada dele = "); scanf("%d",&T);
+	} while ( t < 0 ); 
+	// ter uma consitencia de dados == o dado tem que ser > ou = 0; senao ele recusa; e pede novamente; 
+	while ( i <= 10)
+        {
+	 r = T * i;
+	 printf("\n%d X %d = %d",T,i,r);
+	 i++; // ou i = i + 1; ou i += 1;
+         }
+}
