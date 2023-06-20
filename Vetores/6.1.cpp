788501f799f4d8874a1709c20e973d995619d4f5
Giulio <126623979+Diberardo@users.noreply.github.com>
2023-06-20 14:11:30 -0300

    .

--- Vetores/6.1.cpp
+++ Vetores/6.1.cpp
@@ -1,9 +1,5 @@
-// reajustes dos salarios com vetores
-// tem X funcionarios dentro de uma empresa, se a media dos salarios for maior que 5 mil reajuste de 1.12 senao 1.15; e mostre o resultado;
 #include<stdio.h>
-
 main()
-
 {
 	float sal[100],x,m = 0;
 	int i;
@@ -14,15 +10,19 @@ main()
 		m = m + sal[i];
 	}
 	m = m / 100 ; 
+	
 	if ( m >= 5000) x = 1.12;
 	else x = 1.15;
+	
 	printf("\nMedia do Salario antes do reajuste = %.2f",m);
-	m = 0;
+	
+	m = 0;    // tem que zerar para ver nova média; 
+	
 	for ( i = 0; i < 100; i++)
 	{
 		sal[i] = sal[i] * x;
 		printf("\nSalario reajustado %d = %.2f",i,sal[i]);
-		m = m + sal[1];
+		m = m + sal[i];
 	}
     printf("\nMedia depois do reajuste = %.2f",m);
 }
