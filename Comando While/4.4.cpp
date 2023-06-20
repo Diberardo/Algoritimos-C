94ae414c4455ed524ba56688a1665212918785ed
Giulio <126623979+Diberardo@users.noreply.github.com>
2023-06-20 11:49:24 -0300

    .

--- Comando While/4.4.cpp
+++ Comando While/4.4.cpp
@@ -1,17 +1,16 @@
 #include<stdio.h>
-
 main()
-
 {
 	int i = 10,t,r;
-	
+	do
+	{
 	printf("\nDigite o Numero para ver a tabuada completa dela = "); scanf("%d",&t);
-	
+	} while ( t < 0 );
 	while( i >= 1)
 	{
-	r = t * i;
-	printf("\n%d X %d = %d",t,i,r);
-    i--;
+	 r = t * i;
+	 printf("\n%d X %d = %d",t,i,r);
+         i--; // ou i = i - 1; ou i-=1;
 	}
     return 0;
 }
