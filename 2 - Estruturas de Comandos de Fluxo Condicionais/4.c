f5b340d23e3e4891f4854a9113114a58ecc8d172
Giulio <126623979+Diberardo@users.noreply.github.com>
2024-04-20 20:43:33 -0300

    Update and rename 2.4.cpp to 4.c

---
+++ 2 - Estruturas de Comandos de Fluxo Condicionais/4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	float F, T; 
+	printf("Aulas dadas = ");scanf("%f" , &T);
+	printf("\nFaltas = "); scanf("%f" , &F);
+
+	if (F/T > 0.25)
+	     printf("\nREPROVADO!!!");
+	else 
+	     printf("\nAPROVADO!!!");
+	            
+	printf("\nSua Presença = %.2f", F/T);
+}
