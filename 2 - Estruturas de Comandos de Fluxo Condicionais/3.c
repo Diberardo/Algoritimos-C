9e152fcc1d0441ab1df9bf909a1692698898bc0f
Giulio <126623979+Diberardo@users.noreply.github.com>
2024-04-20 20:41:46 -0300

    Update and rename 2.3.cpp to 3.c

--- 2 - Estruturas de Comandos de Fluxo Condicionais/3.c
+++ 2 - Estruturas de Comandos de Fluxo Condicionais/3.c
@@ -3,27 +3,27 @@ main()
 {
 	int N1, N2, N3, AUX;
 	
-	printf("digite N1 = "); scanf("%d",&N1);
+	printf("\ndigite N1 = "); scanf("%d",&N1);
 	printf("\ndigite N2 = "); scanf("%d",&N2);
 	printf("\ndigite N3 = "); scanf("%d",&N3);
 	
 	   if (N1 > N2) 
 	   {
-	     AUX = N1;
-	   	  N1 = N2;
-	   	   N2 = AUX;
+	        AUX = N1;
+	        N1 = N2;
+	   	N2 = AUX;
 	   }
 	   if (N2 > N3)
 	   {
-	   	 AUX = N2;
-	   	  N2 = N3;
-	   	   N3 = AUX;
+	   	AUX = N2;
+	   	N2 = N3;
+	   	N3 = AUX;
 	   }
 	   if (N1 > N2)
 	   {
-	   	 AUX = N1;
-	   	  N1 = N2;
-	   	    N2 = AUX;
+	   	AUX = N1;
+	   	N1 = N2;
+	   	N2 = AUX;
 	   }
 	printf("\nA ordem Crescente = %d,%d,%d",N1,N2,N3);
 }
