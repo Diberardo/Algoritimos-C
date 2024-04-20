ee1e420f0cd4f07431e399c4fdbb2b1ed70d7fac
Giulio <126623979+Diberardo@users.noreply.github.com>
2024-04-20 20:59:17 -0300

    Update and rename 3.2.cpp to 2.c

--- 3 - Estruturas de comandos de fluxo condicionais aninhadas/2.c
+++ 3 - Estruturas de comandos de fluxo condicionais aninhadas/2.c
@@ -7,10 +7,14 @@ main()
 	 printf("\nSalario Atual = "); scanf("%f",&sal);
 	 printf("\nTempo = "); scanf("%d",&temp);
 	 
-	 if (temp < 5) sf = sal * 1.5;
-	  else if (temp <= 10) sf = sal * 1.10;
-	   else if (temp <= 20) sf = sal * 1.20;
-	    else sf = sal * 1.25;
+	 if (temp < 5) 
+		 sf = sal * 1.5;
+	  else if (temp <= 10) 
+		  sf = sal * 1.10;
+	   else if (temp <= 20) 
+		   sf = sal * 1.20;
+	    else 
+		   sf = sal * 1.25;
 	    
 	 printf("\nO Salario final = %.2f",sf);
 }
