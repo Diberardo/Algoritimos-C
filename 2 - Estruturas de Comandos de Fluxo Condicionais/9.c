e93c09ddc304e3bf578ca593a3f836d65e21d696
Giulio <126623979+Diberardo@users.noreply.github.com>
2024-04-20 20:53:10 -0300

    Update and rename 2.9.cpp to 9.c

---
+++ 2 - Estruturas de Comandos de Fluxo Condicionais/9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+main()
+{
+	int ID;
+	printf("digite sua idade = "); scanf("%d",&ID);
+	  if (ID < 18) 
+		  printf("\nMENOR DE IDADE!!!");
+	   else 
+		  printf("\nMAIOR DE IDADE!!!");
+}
--- Estruturas de Comandos de Fluxo Condicionais/2.9.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-main()
-{
-	int ID;
-	
-	 printf("digite sua idade = "); scanf("%d",&ID);
-	  if (ID < 18) printf("\nMENOR DE IDADE!!!");
-	   else printf("\nMAIOR DE IDADE!!!");
-}
