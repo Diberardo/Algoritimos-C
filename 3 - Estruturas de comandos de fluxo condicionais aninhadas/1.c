62783e2f54c1671356f52354963c772b9eea814d
Giulio <126623979+Diberardo@users.noreply.github.com>
2024-04-20 20:55:02 -0300

    Update and rename 3.1.cpp to 1.c

--- 3 - Estruturas de comandos de fluxo condicionais aninhadas/1.c
+++ 3 - Estruturas de comandos de fluxo condicionais aninhadas/1.c
@@ -3,15 +3,10 @@
 int main()
 {
     int A;
-    printf("\nAno = ");
-    scanf("%d",&A);
+    printf("\nAno = "); scanf("%d",&A);
     if ((A % 4 == 0 && A % 100 != 0) || (A % 400 == 0)) 
-    {
         printf("\nBissexto");
-    } 
-	else 
-	{
+    else 
           printf("\nNao e bissexto");
-        }
     return 0;
 }
