683a0cf067f9560dd9f10b4813dccb4dd32fed6c
Giulio <126623979+Diberardo@users.noreply.github.com>
2024-04-20 20:58:00 -0300

    Update and rename 3.14.cpp to 14.c

---
+++ 3 - Estruturas de comandos de fluxo condicionais aninhadas/14.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	int ld;
+	 printf("\nNumero de lados = "); scanf("%d",&ld);
+	  if (ld < 3) 
+		  printf("\nNao e um poligono");
+	   else if (ld == 3) 
+		   printf("\nE um triangulo");
+	    else if (ld == 4) 
+		    printf("\nE um retangulo");
+	     else if (ld == 5) 
+		     printf("\nE um pentagono");
+	      else if (ld == 6) 
+		      printf("\nE um hexagono");
+	       else if (ld > 6)
+		       printf ("\nIsso esta alem de um Hexagono");
+}
