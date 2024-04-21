eff3fde0070885eba5d3130a65b3e0e40f7804df
Giulio <126623979+Diberardo@users.noreply.github.com>
2024-04-20 21:06:29 -0300

    Update and rename 3.4.cpp to 4.c

---
+++ 3 - Estruturas de comandos de fluxo condicionais aninhadas/4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int A,B;
+	
+	 printf("\nA = "); scanf("%d",&A);
+	 printf("\nB = "); scanf("%d",&B);
+	 
+	  if(A == B) 
+		  printf("\nSao iguais");
+	   else if (A > B) 
+		   printf("\nA Maior");
+	    else 
+		   printf("\nB Maior");
+}
