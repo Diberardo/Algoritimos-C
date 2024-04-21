0dc2f669146701a1c215765f241eb0159e790d9c
Giulio <126623979+Diberardo@users.noreply.github.com>
2024-04-21 20:28:57 -0300

    Update and rename 6.6.cpp to 6.c

--- 6 - Vetores/6.c
+++ 6 - Vetores/6.c
@@ -9,17 +9,17 @@ main()
 	int v[n * 2 - 1]; // tem que dobrar, eu pedi as cidades e falta o km 
 	do
 	{
-		printf("\nPosição da cidade 0 "); scanf("%d",&v[0]);
+		printf("\nPosiÃ§Ã£o da cidade 0 "); scanf("%d",&v[0]);
 	} while ( v[0] <= 0 );
 	for ( i = 2; i < n * 2 - 1; i += 2 )
 	{
 		do
 		{
-			printf("\nPosição da cidade %d = ",i); scanf("%d",&v[i]);
+			printf("\nPosiÃ§Ã£o da cidade %d = ",i); scanf("%d",&v[i]);
 		} while ( v[i] <= v[i - 2] );
 		v[i - 1] = v[i] - v[ i - 2 ];
 	}
-	for ( i = 0; i < n * 2 - 1; i++ ) // -1 porque o vetor começa em 0 tem que tirar o ultimo km 
+	for ( i = 0; i < n * 2 - 1; i++ ) // -1 porque o vetor comeÃ§a em 0 tem que tirar o ultimo km 
 	{
 		if ( i % 2 == 0 )
 		printf("\tCidade %d = %d",i,v[i]);
